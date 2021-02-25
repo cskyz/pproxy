672d5c5581d27ea2506946affdaf0c881f52f417
cskyz <codymax.z@gmail.com>
2021-02-25 19:56:41 +0800

    Update request.rs
    remove

--- src/request.rs
+++ src/request.rs
@@ -31,68 +31,7 @@ impl HTTPRequest {
         }
         None
     }
-/*
-    /// Build a message for this HTTPRequest.
-    /// The return data can be sent to the server.
-    pub fn build_message(&self) -> Bytes {
-        let mut ret = String::new();
-        let command_line = format!("{} {} {}\r\n", self.method, self.path, self.protocol);
-        ret.push_str(&command_line);
-        for (key, value) in &self.headers {
-            match key.as_str() {
-                "Connection" => ret.push_str("Connection: close\r\n"),
-                "Proxy-Connection" => {}
-                _ => {
-                    ret.push_str(&format!("{}: {}\r\n", key, value));
-                }
-            }
-        }
-        ret.push_str("\r\n");
-        Bytes::from(ret)
-    }
 
-    /// Build a new HTTPRequest based on this request as a proxy.
-    ///
-    /// The new request should be sent to real destination of this request
-    /// and indicated the client is a proxy.
-    pub fn build_request_for_proxy(&self) -> Self {
-        if !self.path.starts_with("http://") {
-            panic!("Not a http request for proxy!");
-        }
-        let path_url = Url::parse(&self.path).unwrap();
-        {
-            let path = match path_url.query() {
-                Some(query) => format!("{}?{}", path_url.path(), query),
-                None => String::from(path_url.path()),
-            };
-            let mut headers = Vec::new();
-            let host = self
-                .get_header_value("Host")
-                .unwrap_or(path_url.host_str().unwrap());
-            headers.push((String::from("Host"), String::from(host)));
-            for (key, value) in &self.headers {
-                match key.as_str() {
-                    "Host" => {}
-                    "Connection" => {}
-                    "Proxy-Connection" => {}
-                    _ => {
-                        headers.push((key.clone(), value.clone()));
-                    }
-                }
-            }
-            //headers.push((String::from("Connection"), String::from("close")));
-            //headers.push((String::from("Proxy-Connection"), String::from("close")));
-            HTTPRequest {
-                method: String::from(&self.method),
-                protocol: String::from("HTTP/1.1"),
-                path,
-                headers,
-                body: self.body.clone(),
-                is_tunnel: false,
-            }
-        }
-    }
-*/
     /// Parsing the message of a request to create an HTTPRequest
     pub fn parse_message(buf: &Bytes) -> Option<Self> {
         let mut pointer = 0;
